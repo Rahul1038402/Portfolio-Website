e1c556dbab9f18f9f02bc4614661617c31c3ec8a
Rahul1038402 <rahul1038402@gmail.com>
2025-11-27 18:32:20 +0530

    new: added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import { Home } from "./Pages/Home"
 import { NotFound } from "./Pages/NotFound"
 import './index.css'
 import { Toaster } from "@/Components/ui/toaster";
+import { Analytics } from '@vercel/analytics/react';
 
 export const App = () => {
   return (
     <>
-    <Toaster/>
+      <Toaster/>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home/>}></Route>
           <Route path="*" element={<NotFound/>}></Route>
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   )
 }
